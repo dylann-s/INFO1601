fa6815213e60ca954b01708895d47da708f51bde
dylann-s <157635473+dylann-s@users.noreply.github.com>
2024-04-19 03:03:43 -0400

    Create firebaseConfig.js

---
+++ public/firebaseConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAyjtUDkdHKS9qHNb70Le5ebFyb7b1nTbQ",
+  authDomain: "info1601-5d056.firebaseapp.com",
+  projectId: "info1601-5d056",
+  storageBucket: "info1601-5d056.appspot.com",
+  messagingSenderId: "603045611166",
+  appId: "1:603045611166:web:c24cd44220004d949c5d8f",
+  measurementId: "G-6Q80D1X86J"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
